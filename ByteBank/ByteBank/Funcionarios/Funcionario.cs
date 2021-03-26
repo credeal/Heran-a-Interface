69a5cd4c91c2848b803e8a78164d464148081522
Fulano <detias@outlook.com>
2021-03-26 14:27:02 -0300

    Projeto Inicial

---
+++ ByteBank/ByteBank/Funcionarios/Funcionario.cs
@@ -0,0 +1,36 @@
+﻿namespace ByteBank.Funcionarios
+{
+    public class Funcionario
+    {
+        //0 - Funcionário
+        //1 - Diretor
+        //2 - Designer
+        //N - ...
+
+        #region Atributos
+        private int _tipo;
+
+
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public double Salario { get; set; }
+        #endregion
+
+        #region Construtor
+        public Funcionario(int Tipo)
+        {
+            _tipo = Tipo;
+        }
+        #endregion
+
+        #region Métodos
+        public double GetBonificacao()
+        {
+            if (_tipo == 1)
+                return Salario;
+
+            return Salario * 0.10;
+        }
+        #endregion
+    }
+}
---
+++ ByteBank/ByteBank/Program.cs
@@ -0,0 +1,18 @@
+﻿using ByteBank.Funcionarios;
+using System;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario Carlos = new Funcionario(1);
+            Carlos.Nome = "Carlos";
+            Carlos.CPF = "444.444.222.01";
+            Carlos.Salario = 1000.00;
+
+            Console.WriteLine(Carlos.GetBonificacao());
+        }
+    }
+}
